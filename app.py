56c9e76a55dbbbccfa3061f7fa3e324111cd8e1e
Pooja Ellanti <pe4111@srmist.edu.in>
2025-07-08 18:03:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def index():
     return render_template("index.html", chart=chart, news=news or [], prediction=prediction, symbol=symbol)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
